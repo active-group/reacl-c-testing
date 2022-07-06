b7452531724d081630378a9a41265a4759a246d8
David Frese <david.frese@active-group.de>
2022-07-06 18:06:01 +0200

    Inital commit (moved from reacl-c project)

---
+++ README.md
@@ -0,0 +1,18 @@
+# Reacl-c-testing
+
+This is a library with utilities to write advanced tests of [Reacl-c](https://github.com/active-group/reacl-c) items and applications.
+It is based on the [React Testing Library](https://testing-library.com/docs/react-testing-library/intro/).
+
+[![Latest Version](https://img.shields.io/clojars/v/de.active-group/reacl-c-testing.svg)](https://clojars.org/de.active-group/reacl-c-testing)
+[![Tests Status](https://github.com/active-group/reacl-c-testing/workflows/Tests/badge.svg)](https://github.com/active-group/reacl-c-testing/actions)
+
+## Documentation
+
+The API documentation for the latest release are available [here](https://cljdoc.xyz/d/de.active-group/reacl-c-testing/CURRENT).
+
+## License
+
+Copyright © 2022 Active Group GmbH
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,39 @@
+(defproject de.active-group/reacl-c-testing "0.1.0-SNAPSHOT"
+  :description "Library with utilities to write advanced tests of Reacl-c items and applications."
+  :url "http://github.com/active-group/reacl-c-testing"
+  
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.773" :scope "provided"]
+                 [de.active-group/reacl-c "0.11.0-SNAPSHOT"]
+                 [de.active-group/active-clojure "0.36.0"]
+                 [de.active-group/cljs-async "2.0.0"]]
+
+  :plugins [[lein-codox "0.10.8"]
+            [lein-auto "0.1.3"]
+            [lein-resource "17.06.1"]]
+
+  :profiles {:shadow {:dependencies [[thheller/shadow-cljs "2.11.7"]
+                                     [binaryage/devtools "1.0.2"]]
+                      :source-paths ["src" "test"]
+                      :resource-paths ["target"]}
+             
+             :codox {:dependencies [[codox-theme-rdash "0.1.2"]]}}
+
+  :clean-targets ^{:protect false} [:target-path]
+
+  :aliases {"dev" ["with-profile" "shadow" "run" "-m" "shadow.cljs.devtools.cli" "watch" "test"]
+            "build-test" ["with-profile" "shadow" "run" "-m" "shadow.cljs.devtools.cli" "compile" "ci"]
+            ;; then run tests with: npx karma start --single-run
+            }
+
+  :codox {:language :clojurescript ;; :clojure
+          :metadata {:doc/format :markdown}
+          :themes [:rdash]
+          :src-dir-uri "http://github.com/active-group/reacl-c-testing/blob/master/"
+          :src-linenum-anchor-prefix "L"}
+
+  :auto {:default {:paths ["src" "test"]}}
+  )
