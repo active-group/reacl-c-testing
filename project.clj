fef5cc586ead91ec73b4459d234b7457b748e364
David Frese <david.frese@active-group.de>
2025-06-26 14:26:15 +0200

    Use newer versions in tests.

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.12.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.12.38" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.773" :scope "provided"]
                  [de.active-group/reacl-c "0.12.1"]
                  [de.active-group/active-clojure "0.36.0"]
                  [de.active-group/cljs-async "2.0.1"]]
@@ -14,7 +14,9 @@
   :plugins [[lein-codox "0.10.8"]
             [lein-auto "0.1.3"]]
 
-  :profiles {:shadow {:dependencies [[thheller/shadow-cljs "3.0.4"]
+  :profiles {:shadow {:dependencies [[org.clojure/clojure "1.12.0"]
+                                     [org.clojure/clojurescript "1.12.38"]
+                                     [thheller/shadow-cljs "3.0.4"]
                                      [binaryage/devtools "1.0.2"]]
                       :source-paths ["src" "test"]
                       :resource-paths ["target"]}
